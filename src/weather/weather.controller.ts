dc76766954c4afd59932f26bc609bb5dda3e674e
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2024-02-16 19:29:28 +0200

    Added global prefix and versioning for the api

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -2,13 +2,13 @@ import { Controller, Get, Logger, Query, UsePipes, ValidationPipe } from '@nestj
 import { WeatherService } from './weather.service';
 import { GetWeatherDto } from './dto/get.weather.dto';
 
-@Controller('weather')
+@Controller('v1')
 export class WeatherController {
   private logger = new Logger('WeatherController');
 
   constructor(private readonly weatherService: WeatherService) {}
 
-  @Get()
+  @Get('weather')
   @UsePipes(new ValidationPipe())
   getWeather(@Query() getWeatherDto: GetWeatherDto) {
     this.logger.log(`Received request for ${getWeatherDto.location}`);
